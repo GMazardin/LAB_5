20ae97814cc119475121326afcc8c3b34b332dd3
Kyriosz <gregoire.mazardin@efrei.net>
2022-02-06 19:22:00 +0100

    Part 3 finished and approvaltest still working

--- GildedRose.cs
+++ GildedRose.cs
@@ -14,74 +14,96 @@ namespace csharp
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                // I chose to create a switch based on the item name
+                // I would have liked to make inheritance, but we couldn't change the "Item.cs" file and I had no idea
+                // on how to make it using only "GildedRose.cs"
+                switch (Items[i].Name)
                 {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                    case "Aged Brie": // Case of the Aged Brie that is different from others
+                        Items[i].SellIn -= 1; // Start by decreasing sellin
+                        if (Items[i].SellIn >= 0) // If its sellin is 0 or above, its quality has to increase by 1
                         {
-                            Items[i].Quality = Items[i].Quality - 1;
+                            Items[i].Quality += 1;
                         }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        else // If sellin is negative, its quality must increase by 2
                         {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
+                            Items[i].Quality += 2;
                         }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        
+                        if (Items[i].Quality > 50) // Security loop check to avoid 50 or above qualities that must be impossible
                         {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
+                            Items[i].Quality = 50;
                         }
+                        break;
+                    
+                    case "Sulfuras, Hand of Ragnaros": // Sulfuras has quality of 80 anytime and never changes
+                        Items[i].Quality = 80;
+                        break;
+                    
+                    case "Backstage passes to a TAFKAL80ETC concert": // Case of Backstage passes that follow particular rules
+                        Items[i].SellIn -= 1; // Start by decreasing sellin
+                        
+                        // If sellin goes from 10 to 9 and is 5 or above, its quality must increase by 2
+                        // I used 9 instead of 10 here because I start be decreasing sellin
+                        if (Items[i].SellIn <= 9 && Items[i].SellIn >= 5)
+                        {
+                            Items[i].Quality += 2;
+                        }
+                        
+                        // If sellin goes from 5 to 4 and is 0 or above, its quality must increase by 3
+                        // Same as before, I used 4 instead of 5 because I start by decreasing sellin
+                        else if (Items[i].SellIn <= 4 && Items[i].SellIn >= 0)
+                        {
+                            Items[i].Quality += 3;
+                        }
+                        
+                        // If sellin is 10 or above, then quality only increases by 1
+                        else if (Items[i].SellIn >= 10)
+                        {
+                            Items[i].Quality += 1;
+                        }
+                        
+                        // If sellin is negative, then quality goes to 0 because concert has passed
                         else
                         {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                            Items[i].Quality = 0;
                         }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
+                        
+                        // Security loop check to avoid 50 or above qualities that must be impossible
+                        if (Items[i].Quality > 50)
                         {
-                            Items[i].Quality = Items[i].Quality + 1;
+                            Items[i].Quality = 50;
                         }
-                    }
+                        break;
+                    
+                    default: // All other objects
+                        Items[i].SellIn -= 1; // Start by decreasing sellin
+                        
+                        // Security loop check to avoid 50 or above qualities that must be impossible
+                        if (Items[i].Quality > 50) 
+                        {
+                            Items[i].Quality = 50;
+                        }
+                        
+                        // If sellin is positive, quality decreases by 1, and if negative it decreases by 2
+                        else if (Items[i].Quality < 50 && Items[i].Quality > 0)
+                        {
+                            if (Items[i].SellIn >= 0)
+                            {
+                                Items[i].Quality -= 1;
+                            }
+                            else
+                            {
+                                Items[i].Quality -= 2;
+                            }
+                        }
+
+                        // Security loop check to avoid negative qualities that must be impossible
+                        if (Items[i].Quality < 0)
+                        {
+                            Items[i].Quality = 0;
+                        }
+                        break;
                 }
             }
         }
