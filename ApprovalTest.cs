7b56db979b43e58965beba3170af82caa39c312f
Kyriosz <gregoire.mazardin@efrei.net>
2022-02-06 18:19:20 +0100

    Part 2 finished and approval test working

--- ApprovalTest.cs
+++ ApprovalTest.cs
@@ -17,11 +17,15 @@ namespace csharp
             StringBuilder fakeoutput = new StringBuilder();
             Console.SetOut(new StringWriter(fakeoutput));
             Console.SetIn(new StringReader("a\n"));
-
             Program.Main(new string[] { });
             var output = fakeoutput.ToString();
-            
-            Approvals.Verify(output);
+
+            // Change here in the approval test :
+            // I saved the output of the program in a txt file, and I compare it to the output of the test
+            // Logically, the approval test should not be changed anymore, as we want still the same result but coded in a better way
+
+            string text = System.IO.File.ReadAllText("../../Test.txt");
+            Assert.AreEqual(output, text);
         }
     }
 }
